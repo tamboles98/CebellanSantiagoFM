0deda3819c0c504589a9b3c493bfc7629f08a0c9
tamboles98 <tamboles98@gmail.com>
2020-10-31 21:29:42 +0100

    Crear el archivo para adquisicion de datos

---
+++ adquisicionDatos.py
@@ -0,0 +1,9 @@
+import requests
+import sys
+
+#Primero el programa necesitara una key para poder acceder a la API
+if len(sys.argv) <= 1:
+    raise TypeError('No se ha introducido key para usar con la API')
+key = sys.argv[1]
+
+r = requests.get('https://euw1.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5', params = {'api_key' : key})
