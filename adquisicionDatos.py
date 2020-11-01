8e0b8f85224134bf89353ffc4b359637e81223d2
tamboles98 <tamboles98@gmail.com>
2020-11-01 21:14:34 +0100

    Recogo el nombre de todos los grandsmasters

--- adquisicionDatos.py
+++ adquisicionDatos.py
@@ -7,3 +7,7 @@ if len(sys.argv) <= 1:
 key = sys.argv[1]
 
 r = requests.get('https://euw1.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5', params = {'api_key' : key})
+
+names = []
+for summoner in r.json()["entries"]:
+    names.append(summoner["summonerName"])
