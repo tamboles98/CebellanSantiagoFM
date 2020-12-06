07158aedd578422aa849f850ddc562260e692390
tamboles98 <tamboles98@gmail.com>
2020-12-06 19:05:40 +0100

    Adquisicion de datos preparado

--- cebellan_santiago_fm/adquisicionDatos.py
+++ cebellan_santiago_fm/adquisicionDatos.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from itertools import chain
 
 
 class request_handler():
@@ -10,95 +11,110 @@ class request_handler():
                 "KR": "kr.api.riotgames.com", #Korea
                 "NA": "na1.api.riotgames.com", #North America
                 "EUNE": "eun1.api.riotgames.com"} #Europe Nordic & East
-    
     LIMITSHORT = (20, 1)
     LIMITLONG = (100, 120)
     
     def __init__(self, api_key):
-        self.short = time.time()
-        self.long = time.time()
-        self.short_n = {"EUW": 0, "KR": 0, "NA": 0, "EUNE": 0}
-        self.long_n = {"EUW": 0, "KR": 0, "NA": 0, "EUNE": 0}
+        ti = time.time()
+        self.short = {"EUW": (0, ti), "KR": (0, ti),
+                        "NA": (0, ti), "EUNE": (0, ti)}
+        self.long = {"EUW": (0, ti), "KR": (0, ti),
+                        "NA": (0, ti), "EUNE": (0, ti)}
         self.api_key = api_key
 
 
-
-    def get_diamond_i(self, routing):
-        url = request_handler.prepare_url(routing = request_handler.ROUTES[routing],
+    def get_diamond_i(self, server):
+        url = request_handler.prepare_url(server = request_handler.ROUTES[server],
             endpoint="/lol/league/v4/entries/RANKED_SOLO_5x5/DIAMOND/I?page=1")
         headers = {
             'X-Riot-Token': self.api_key
             }
 
-        return requests.request("GET", url, headers=headers)
+        return self.safe_request(server=server, url=url, headers=headers)
 
 
-    def get_account_id(self, route, summoner_id):
+    def get_account_id(self, server, summoner_id):
         endpoint = "/lol/summoner/v4/summoners/{}".format(summoner_id)
-        url = request_handler.prepare_url(routing = request_handler.ROUTES[route],
+        url = request_handler.prepare_url(server = request_handler.ROUTES[server],
             endpoint= endpoint)
         headers = {
             'X-Riot-Token': self.api_key
             }
         
-        return requests.request("GET", url, headers=headers)
+        return self.safe_request(server=server, url=url, headers=headers)
 
     
-    def get_match_history(self, route, account_id):
+    def get_match_history(self, server, account_id):
         endpoint = "/lol/match/v4/matchlists/by-account/{}".format(account_id)
-        url = request_handler.prepare_url(routing = request_handler.ROUTES[route],
+        url = request_handler.prepare_url(server = request_handler.ROUTES[server],
             endpoint= endpoint)
         headers = {
             'X-Riot-Token': self.api_key
             }
         
-        return requests.request("GET", url, headers=headers)
-
+        return self.safe_request(server=server, url=url, headers=headers)
     
-    def get_match(self, route, match_id):
+    def get_match(self, server, match_id):
         endpoint = "/lol/match/v4/matches/{}".format(match_id)
-        url = request_handler.prepare_url(routing = request_handler.ROUTES[route],
+        url = request_handler.prepare_url(server = request_handler.ROUTES[server],
             endpoint= endpoint)
         headers = {
             'X-Riot-Token': self.api_key
             }
         
-        return requests.request("GET", url, headers=headers)
+        return self.safe_request(server=server, url=url, headers=headers)
 
 
     @staticmethod
-    def prepare_url(routing, endpoint):
-        return "https://" + routing + endpoint
+    def prepare_url(server, endpoint):
+        return "https://" + server + endpoint
 
-    
-    def safe_request(self, route, url, headers):
-        if (self.short_n[route] < request_handler.LIMITSHORT[0] and
-            self.long_n[route] < request_handler.LIMITLONG[0]):
-            self.short_n[route] += 1
-            self.long_n[route] += 1
+    #A partir los metodos sirven para asegurarse que no se hacen demasiadas
+    # requests a ningún server.
+
+    def safe_request(self, server, url, headers):
+        if self.ready_for_request(server):
+            self.short[server][0] += 1
+            self.long[server][0] += 1
             return requests.request("GET", url, headers=headers)
         #Si hemos alcanzado algún limite comprueba que no sea hora de
         # resetear los tiempos
-        self.check_timers_reset()
-        if (self.short_n[route] < request_handler.LIMITSHORT[0] and
-            self.long_n[route] < request_handler.LIMITLONG[0]):
-            self.short_n[route] += 1
-            self.long_n[route] += 1
+        if self.check_timers_reset(server) and self.ready_for_request(server):
+            self.short_n[server] += 1
+            self.long_n[server] += 1
             return requests.request("GET", url, headers=headers)
     
-    def check_timers_reset(self):
-        if time.time() - self.short > request_handler.LIMITSHORT[1]:
-            self.short_n = {"EUW": 0, "KR": 0, "NA": 0, "EUNE": 0}
-            self.short = time.time()
-        if time.time() - self.long > request_handler.LIMITLONG[1]:
-            self.long_n = {"EUW": 0, "KR": 0, "NA": 0, "EUNE": 0}
-            self.long = time.time()
+    #Este metodo comprueba si ya ha pasado tiempo suficiente desde el ultimo
+    # reset para el server y resetea si ese es el caso.
+    def check_timers_reset(self, server):
+        ti = time.time() #Current time
+        change = False #Comprueba si ha habido algún cambio al ejecutar esta
+                        # acción
+        if ti - self.short[server][1] > request_handler.LIMITSHORT[1]:
+            self.short[server] = (0, ti)
+            change = True
+        if ti - self.long[server][1] > request_handler.LIMITLONG[1]:
+            self.long[server] = (0, ti)
+            change = True
+        return change
+
+    #Sleeps hasta que sea hora de resetear
+    def wait_for_reset(self):
+        ti = time.time()
+        waitime = (request_handler.LIMITLONG[1] -
+            min(ti - i[1] for i in self.long.values()))
+        time.sleep(waitime)
+        time.sleep(1)
+        ti = time.time()
+        self.short = {"EUW": (0, ti), "KR": (0, ti),
+                        "NA": (0, ti), "EUNE": (0, ti)}
+        self.long = {"EUW": (0, ti), "KR": (0, ti),
+                        "NA": (0, ti), "EUNE": (0, ti)}
         return None
 
-    
-    def check_sleep(self):
-        if (all(i >= request_handler.LIMITSHORT[0] for i in self.short_n.values())
-            and 
-            all(i >= request_handler.LIMITLONG[0] for i in self.long_n.values())):
+    def ready_for_request(self, server):
+        return (self.short[server][0] < request_handler.LIMITSHORT[0] and
+        self.long[server][0] < request_handler.LIMITLONG[0])
+
+
         
-            
